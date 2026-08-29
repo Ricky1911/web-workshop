f859dec83538c3d528d9cb2d035fba8e22b65b75
Ricky1911 <88931060+Ricky1911@users.noreply.github.com>
2026-08-29 18:22:22 +0800

    feat(03): add database support for note

---
+++ database/sql/note.sql
@@ -0,0 +1,10 @@
+create table if not exists public.note (
+  user_uuid  uuid   not null,
+  room_uuid  uuid   not null,
+  content    text   not null,
+  created_at timestamp default current_timestamp not null,
+  updated_at timestamp default current_timestamp not null,
+  primary key (user_uuid, room_uuid),
+  foreign key (user_uuid) references public.user (uuid) on update cascade on delete cascade,
+  foreign key (room_uuid) references public.room (uuid) on update cascade on delete cascade
+);
