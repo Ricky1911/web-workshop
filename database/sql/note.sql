5d6c872088f5ea16e8e613f4bbc112ccea0c213f
Ricky1911 <88931060+Ricky1911@users.noreply.github.com>
2026-08-29 18:31:02 +0800

    fix(03): fix inconsistent file status

--- database/sql/note.sql
+++ database/sql/note.sql
@@ -3,7 +3,6 @@ create table if not exists public.note (
   room_uuid  uuid   not null,
   content    text   not null,
   created_at timestamp default current_timestamp not null,
-  updated_at timestamp default current_timestamp not null,
   primary key (user_uuid, room_uuid),
   foreign key (user_uuid) references public.user (uuid) on update cascade on delete cascade,
   foreign key (room_uuid) references public.room (uuid) on update cascade on delete cascade
